86a71ab87e3e895a478c8380e7c7a9a5f79f4d6b
adrianaardi <adrianaardi.aa@gmail.com>
2026-05-08 14:30:24 +0800

    migration fix

--- database/migrations/2026_04_06_090545_add_role_to_users_table.php
+++ database/migrations/2026_04_06_090545_add_role_to_users_table.php
@@ -6,16 +6,14 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('user');
-        });
+        if (!Schema::hasColumn('users', 'role')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('role')->default('user');
+            });
+        }
     }
-
     /**
      * Reverse the migrations.
      */
