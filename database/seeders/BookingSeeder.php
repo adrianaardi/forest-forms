f1a11d6ef83d47beb88a923ce61f3a29c1e641a5
Nur Shakira <shakirabaher08@gmail.com>
2026-06-26 16:36:01 +0800

    add RDD rooms

--- database/seeders/BookingSeeder.php
+++ database/seeders/BookingSeeder.php
@@ -9,39 +9,16 @@ class BookingSeeder extends Seeder
 {
     public function run(): void
     {
-        $ibuPejabat = \App\Models\Wilayah::where('nama_wilayah', 'Ibu Pejabat')->first();
-        $miri       = \App\Models\Wilayah::where('nama_wilayah', 'Miri')->first();
-        $bintulu    = \App\Models\Wilayah::where('nama_wilayah', 'Bintulu')->first();
-        $sibu       = \App\Models\Wilayah::where('nama_wilayah', 'Sibu')->first();
-        $limbang    = \App\Models\Wilayah::where('nama_wilayah', 'Limbang')->first();
-        $lawas      = \App\Models\Wilayah::where('nama_wilayah', 'Lawas')->first();
-        $kapit      = \App\Models\Wilayah::where('nama_wilayah', 'Kapit')->first();
-        $sarikei    = \App\Models\Wilayah::where('nama_wilayah', 'Sarikei')->first();
-        $kuching    = \App\Models\Wilayah::where('nama_wilayah', 'Kuching')->first();
+	    $rdd = \App\Models\Wilayah::where('nama_wilayah', 'RDD')->first();
 
         $rooms = [
-            // Ibu Pejabat
-            ['nama_bilik' => 'Ramin',        'aras' => 'Aras 11', 'wing' => 'Left Wing',  'wilayah_id' => $ibuPejabat?->id],
-            ['nama_bilik' => 'Rengas',       'aras' => 'Aras 11', 'wing' => 'Right Wing', 'wilayah_id' => $ibuPejabat?->id],
-            ['nama_bilik' => 'Bindang',      'aras' => 'Aras 12', 'wing' => 'Right Wing', 'wilayah_id' => $ibuPejabat?->id],
-            ['nama_bilik' => 'Menggeris',    'aras' => 'Aras 13', 'wing' => 'Right Wing', 'wilayah_id' => $ibuPejabat?->id],
-            ['nama_bilik' => 'Ruang Bengkel','aras' => 'Aras 13', 'wing' => 'Right Wing', 'wilayah_id' => $ibuPejabat?->id],
-            ['nama_bilik' => 'Jongkong',     'aras' => 'Aras 13', 'wing' => 'Right Wing', 'wilayah_id' => $ibuPejabat?->id],
-            ['nama_bilik' => 'Engkabang',    'aras' => 'Aras 14', 'wing' => 'Left Wing',  'wilayah_id' => $ibuPejabat?->id],
-            ['nama_bilik' => 'Selangan',     'aras' => 'Aras 15', 'wing' => 'Right Wing', 'wilayah_id' => $ibuPejabat?->id],
-            ['nama_bilik' => 'Belian',       'aras' => 'Aras 15', 'wing' => 'Left Wing',  'wilayah_id' => $ibuPejabat?->id],
-
-            // Other wilayahs
-            ['nama_bilik' => 'Bilik Mesyuarat', 'aras' => 'Tingkat 6', 'wing' => '-', 'wilayah_id' => $miri?->id],
-            ['nama_bilik' => 'Sri Belian',                'aras' => 'Tingkat 3',         'wing' => '-', 'wilayah_id' => $bintulu?->id],
-            ['nama_bilik' => 'Bilik Mesyuarat','aras' => 'Tingkat 12','wing' => '-', 'wilayah_id' => $sibu?->id],
-            ['nama_bilik' => 'Bilik Mesyuarat','aras' => 'Tingkat 13','wing' => '-', 'wilayah_id' => $sibu?->id],
-
-            ['nama_bilik' => 'Nepenthes',                 'aras' => 'Tingkat 3',         'wing' => '-', 'wilayah_id' => $limbang?->id],
-            ['nama_bilik' => 'Bilik Mesyuarat', 'aras' => 'Tingkat 2', 'wing' => '-', 'wilayah_id' => $lawas?->id],
-            ['nama_bilik' => 'Bilik Mesyuarat', 'aras' => 'Tingkat 5', 'wing' => '-', 'wilayah_id' => $kapit?->id],
-            ['nama_bilik' => 'Bilik Mesyuarat', 'aras' => 'Tingkat 5', 'wing' => '-', 'wilayah_id' => $sarikei?->id],
-            ['nama_bilik' => 'Bilik Mesyuarat', 'aras' => 'Tingkat 1', 'wing' => '-', 'wilayah_id' => $kuching?->id],
+            ['nama_bilik' => 'Bilik Mesyuarat A',        'aras' => 'Tingkat 1', 'wing' => '-', 'wilayah_id' => $rdd?->id],
+            ['nama_bilik' => 'Bilik Mesyuarat Mycology', 'aras' => 'Tingkat 1', 'wing' => '-', 'wilayah_id' => $rdd?->id],
+            ['nama_bilik' => 'Bilik Mesyuarat Entomology','aras' => 'Tingkat 1', 'wing' => '-', 'wilayah_id' => $rdd?->id],
+            ['nama_bilik' => 'Bilik Mesyuarat Botany',    'aras' => 'Tingkat 1', 'wing' => '-', 'wilayah_id' => $rdd?->id],
+            ['nama_bilik' => 'Bilik Mesyuarat Paedology', 'aras' => 'Tingkat 1', 'wing' => '-', 'wilayah_id' => $rdd?->id],
+            ['nama_bilik' => 'Bilik Latihan',             'aras' => 'Tingkat 1', 'wing' => '-', 'wilayah_id' => $rdd?->id],
+            ['nama_bilik' => 'Dewan Serbaguna RDD',       'aras' => 'Tingkat 1', 'wing' => '-', 'wilayah_id' => $rdd?->id],
         ];
 
         foreach ($rooms as $room) {
--- database/seeders/WilayahSeeder.php
+++ database/seeders/WilayahSeeder.php
@@ -10,16 +10,8 @@ class WilayahSeeder extends Seeder
     public function run(): void
     {
         DB::table('wilayahs')->insert([
-            ['nama_wilayah' => 'Ibu Pejabat'],
-            ['nama_wilayah' => 'Kuching'],
-            ['nama_wilayah' => 'Sibu'],
-            ['nama_wilayah' => 'Miri'],
-            ['nama_wilayah' => 'Bintulu'],
-            ['nama_wilayah' => 'Sri Aman'],
-            ['nama_wilayah' => 'Sarikei'],
-            ['nama_wilayah' => 'Kapit'],
-            ['nama_wilayah' => 'Limbang'],
-            ['nama_wilayah' => 'Lawas'],
+            ['nama_wilayah' => 'RDD'],
+
         ]);
     }
 }
